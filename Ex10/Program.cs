c61d3de300d1203f70293c09a442d60c85e4b02a
Nikita <larin.nekit@mail.ru>
2022-12-27 22:57:31 +0300

    10 Задача

---
+++ Ex10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Введи 3-х значное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("2-ая цифра этого числа -> "+stringNumber[1]);
+
