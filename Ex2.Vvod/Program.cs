0751bc64b3119faa9ba7abe772abd1e09ba05945
Nikita <larin.nekit@mail.ru>
2022-12-27 22:39:05 +0300

    VVod

---
+++ Ex2.Vvod/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет,дорогой друг");
+Console.Write(username);   
