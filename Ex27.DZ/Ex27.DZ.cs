6fd1ca2bfd49aaa7616b7702493ee23d98eeb81a
Nikita <larin.nekit@mail.ru>
2022-12-29 12:59:22 +0300

    27-ая задача

---
+++ Ex27.DZ/Ex27.DZ.cs
@@ -0,0 +1,21 @@
+﻿//Выполнении задачи номер 27 - 
+
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
