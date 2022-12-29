62f4e4f9def6cdd5ef3598d5b16c8d63f89ec342
Nikita <larin.nekit@mail.ru>
2022-12-29 11:11:59 +0300

    21 задача - поменял координаты( на n,e,w)Хотел сделать New Year)

---
+++ Ex21.DZ/Ex21.DZ.cs
@@ -0,0 +1,27 @@
+﻿//Задача 21.Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.//
+//x = n ,y = e , z = w -  Хотел немного прикольно сделать - задумка не получилась)
+int n1 = Coordinate("n", "A");
+int e1 = Coordinate("e", "A");
+int w1 = Coordinate("w", "A");
+int n2 = Coordinate("n", "B");
+int e2 = Coordinate("e", "B");
+int w2 = Coordinate("w", "B");
+
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double n1, double n2, 
+                double e1, double e2, 
+                double w1, double w2){
+  return Math.Sqrt(Math.Pow((n2-n1), 2) + 
+                   Math.Pow((e2-e1), 2) + 
+                   Math.Pow((w2-w1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(n1, e2, w1, n2, e1, w2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
