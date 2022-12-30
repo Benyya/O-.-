1e4df3bf2537c846c3469860e8ce6d72a8bf7800
Nikita <larin.nekit@mail.ru>
2022-12-30 23:24:04 +0300

    Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.

---
+++ Ex58.DZ/Ex58.DZ.cs
@@ -0,0 +1,57 @@
+﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5); 
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+int size = InputInt("Размер:");
+int[,] matrixA = new int[size, size];
+int[,] matrixB = new int[size, size];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+int[,] matrixC = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int k = 0; k < size; k++)
+        {
+            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+        }
+    }
+}
+Console.WriteLine("Матрица - А");
+PrintArray(matrixA);
+Console.WriteLine();
+Console.WriteLine("Матрица - В");
+PrintArray(matrixB);
+Console.WriteLine();
+Console.WriteLine("Произведение матриц А*В");
+PrintArray(matrixC);
