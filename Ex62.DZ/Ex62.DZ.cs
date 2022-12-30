5e3e90c0b9a5458ebedab237c37ed44fa2c1cee5
Nikita <larin.nekit@mail.ru>
2022-12-31 00:19:54 +0300

    Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.

---
+++ Ex62.DZ/Ex62.DZ.cs
@@ -0,0 +1,39 @@
+﻿//Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Console.WriteLine("Размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[,] nums = new int[size, size];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= size * size)
+{
+    nums[i, j] = num;
+    if (i <= j + 1 && i + j < size - 1)
+        ++j;
+    else if (i < j && i + j >= size - 1)
+        ++i;
+    else if (i >= j && i + j > size - 1)
+        --j;
+    else
+        --i;
+    ++num;
+}
+
+PrintArray(nums);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
