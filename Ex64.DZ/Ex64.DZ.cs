7c78157660e12d088103abbe73d66b7333f4d568
Nikita <larin.nekit@mail.ru>
2022-12-31 10:46:18 +0300

    Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии

---
+++ Ex64.DZ/Ex64.DZ.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+int n = InputInt("Наберите положительное число : ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не положительное число :");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
